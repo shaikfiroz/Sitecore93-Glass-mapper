55bdf696d7e0a9ec7c5c1cb764ed54ee854e7a01
shaikfiroz <firojahammads@gmail.com>
2021-08-09 18:15:05 +0530

    Add files via upload

---
+++ GlassCustomConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using Devpracticesc93.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Devpracticesc93
+{
+	public class GlassCustomConfigurator : IServicesConfigurator
+	{
+		public void Configure(IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddTransient<ArticleController>();
+			serviceCollection.AddTransient<PracticeController>();
+		}
+	}
+}
